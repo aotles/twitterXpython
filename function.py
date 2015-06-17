1fbeefc9e3c6ac27f0fd2354e5d2a84d7907e6c8
Arel Otles <aotles@gmail.com>
2015-06-17 11:12:44 -0500

    fixing security

--- function.py
+++ function.py
@@ -1,10 +1,10 @@
 import tweepy
 
 def authenticate():
-	consumer_key="m5Gp6Dsn0XSOoLDDiuM7YWjvg"
-	consumer_secret="jcGeb8P1g3nWpBWBx5XNtGw6m2QLCrJidfk7r8Kdj36gf67u1g"
-	access_token="916209919-kNU2JFW6MGA9UscJRQ3u91Gcn409anMhoXvgwO1M"
-	access_token_secret="VIYiNfjIa6xBNrePgxStoFEHxZC39QYh7ycCHgxVkGbUr"
+	consumer_key="XXXXXX"
+	consumer_secret="XXXXXX"
+	access_token="XXXXXX"
+	access_token_secret="XXXXXX"
 	auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 	auth.secure = True
 	auth.set_access_token(access_token, access_token_secret)
